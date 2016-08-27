69eb20bbc8b8c184d0d42f58ed9594e37d25f071
pengsida <291277604@qq.com>
2016-08-27 11:08:50 +0800

    first xcode commit

--- mini_stl/mini_stl/main.cpp
+++ mini_stl/mini_stl/main.cpp
@@ -8,8 +8,10 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
     return 0;
 }
