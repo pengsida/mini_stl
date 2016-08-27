199a514ddc84398400da30a0b4dee4ee8b0298bb
pengsida <291277604@qq.com>
2016-08-27 11:03:21 +0800

    test

---
+++ mini_stl/mini_stl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mini_stl
+//
+//  Created by pengsida on 16/8/27.
+//  Copyright © 2016年 pengsida. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
